e1fcc7c948f528da5bc416a1033b945d80f7a9a1
Andressosa8u8 <bryan.sosa@ecu911.gob.ec>
2024-09-10 08:40:59 -0500

    Codigo Valido con BDD

--- Ecu911Pasantes/views/admin/admin.Master.cs
+++ Ecu911Pasantes/views/admin/admin.Master.cs
@@ -11,28 +11,28 @@ namespace Ecu911Pasantes.views.admin
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-            //if (!IsPostBack)
-            //{
-            //    if (Session["Admin"] != null)
-            //    {
-            //        string usulogeado = Session["Admin"].ToString();
-            //        string resNom = Session["nombre"].ToString();
-            //        string resApe = Session["apellido"].ToString();
-            //        string resRol = Session["rol"].ToString();
-            //        lblNombre.Text = resNom + " " + resApe;
-            //        //lblRol.Text = resRol;
-            //    }
-            //    else
-            //    {
-            //        Response.Redirect("../../autentificación/index.aspx");
-            //    }
-            //}
+            if (!IsPostBack)
+            {
+                if (Session["Admin"] != null)
+                {
+                    string usulogeado = Session["Admin"].ToString();
+                    string resNom = Session["nombre"].ToString();
+                    string resApe = Session["apellido"].ToString();
+                    string resRol = Session["rol"].ToString();
+                    lblNombre.Text = resNom + " " + resApe;
+                    //lblRol.Text = resRol;
+                }
+                else
+                {
+                    Response.Redirect("../../autentificación/index.aspx");
+                }
+            }
         }
 
         protected void lnbLogout_Click(object sender, EventArgs e)
         {
-            //Session.RemoveAll();
-            //Response.Redirect("../../autentificación/index.aspx");
+            Session.RemoveAll();
+            Response.Redirect("../../autentificación/index.aspx");
         }
     }
 }
